162c203c2408680bc7f3b8633655917af7172d8f
Uday <json90327@gmail.com.id>
2026-05-03 22:02:37 +0530

    "uday"

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import State2 from './Usestate2/State2'
+import State3 from './Usestate2/State3'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-<State2/>
+<State3/>
   </StrictMode>,
 )
