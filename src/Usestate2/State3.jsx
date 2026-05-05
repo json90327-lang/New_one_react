a5d787c3b734bd6883ea9689cf87d447b395c3cd
Uday <json90327@gmail.com.id>
2026-05-05 10:31:05 +0530

    "uday"

--- src/Usestate2/State3.jsx
+++ src/Usestate2/State3.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { useState } from 'react';
 
 const State3 = () => {
+    const arr=[
+        {
+            firstname:"uday",
+            lastname:"Narashiha",
+            age:"18",
+        },
+        {
+            firstname:"prasad",
+            lastname:"Bommalatodu pilla baccha",
+            age:17,
+        },
+    ];
+    const [data,setdata]=useState(arr);
+    console.log(data);
   return (
-    <div>State3</div>
+    <div>
+        <ul>
+            {data.map((eachItem,index)=>{
+                const{firstname,lastname,age}=eachItem;
+                return(
+                    <li key={index}>
+                        <div>my first name: <strong>{firstname}</strong></div>
+                        <div>my last name : <strong>{lastname}</strong></div>
+                        <div>my age :<strong>{age}</strong></div>
+                    </li>
+                );
+            })}
+        </ul>
+    </div>
   )
 }
-
 export default State3
